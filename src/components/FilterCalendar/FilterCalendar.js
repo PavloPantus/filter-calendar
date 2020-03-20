10d17d1f3d65cd7dd05072ac9a847b056788d8db
PavloPantus <mr.pantus@gmail.com>
2020-03-20 14:44:59 +0200

    calendar styles added

--- src/components/FilterCalendar/FilterCalendar.js
+++ src/components/FilterCalendar/FilterCalendar.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import './FilterCalendar.scss';
 import SimpleReactCalendar from 'simple-react-calendar';
+import moment from 'moment';
 
 const FilterCalendar = () => {
-  const [date, setDate] = useState(new Date());
+  let now = moment();
+
+  const [date, setDate] = useState(new Date);
   const [selectStart, setSelectStart] = useState(undefined);
   const [selectEnd, setSelectEnd] = useState(undefined);
+  const [month, setMonth] = useState((new Date).getMonth());
+
+  let allMonth = ['Январь' , 'Февраль' , 'Март' , 'Апрель' , 'Май' , 'Июнь' , 'Июль' , 'Август' , 'Сентябрь' , 'Октябрь' , 'Ноябрь' , 'Декабрь'];
 
   return (
     <div className="calendar">
@@ -18,42 +24,105 @@ const FilterCalendar = () => {
         </label>
 
         <label>
-          <input className="calendar__filter-checkbox" value="today" name="active-filter" type="radio" />
+          <input
+            onChange={()=>{
+              setSelectStart(new Date); setSelectEnd(new Date)
+              setMonth((new Date).getMonth());
+              setDate(new Date)
+            }}
+            className="calendar__filter-checkbox"
+            value="today"
+            name="active-filter"
+            type="radio"
+          />
           <div className="calendar__filter-period">
             Сегодня
           </div>
         </label>
 
         <label>
-          <input className="calendar__filter-checkbox" value="yesterday" name="active-filter" type="radio" />
+          <input
+            onChange={()=>{
+              let yesterday = now.subtract(1,'day')._d;
+              setSelectStart(yesterday);
+              setSelectEnd(yesterday)
+              setMonth((new Date).getMonth());
+              setDate(new Date)
+            }
+            }
+            className="calendar__filter-checkbox" value="yesterday" name="active-filter" type="radio"
+          />
           <div className="calendar__filter-period">
             Вчера
           </div>
         </label>
 
         <label>
-          <input className="calendar__filter-checkbox" value="lastSevenDays" name="active-filter" type="radio" />
+          <input
+            onChange={()=>{
+              let sevenDaysAgo = now.subtract(7,'day')._d;
+              setSelectStart(sevenDaysAgo);
+              setSelectEnd(new Date)
+              setMonth((new Date).getMonth());
+              setDate(new Date)
+            }
+            }
+            className="calendar__filter-checkbox" value="lastSevenDays" name="active-filter" type="radio" />
           <div className="calendar__filter-period">
             Последние 7 дней
           </div>
         </label>
 
         <label>
-          <input className="calendar__filter-checkbox" value="lastThirtyDays" name="active-filter" type="radio" />
+          <input
+            onChange={()=>{
+              let thirtyDaysAgo = now.subtract(30,'day')._d;
+              setSelectStart(thirtyDaysAgo);
+              setSelectEnd(new Date)
+              setMonth((new Date).getMonth());
+              setDate(new Date)
+            }
+            }
+            className="calendar__filter-checkbox" value="lastThirtyDays" name="active-filter" type="radio"
+          />
           <div className="calendar__filter-period">
             Последние 30 дней
           </div>
         </label>
 
         <label>
-          <input className="calendar__filter-checkbox" value="duringThisMonth" name="active-filter" type="radio" />
+          <input
+            onChange={()=>{
+              let startOfthisMonth = now.startOf('month')._d.toString();
+              let endOfthisMonth = now.endOf('month')._d.toString();
+              setSelectStart(new Date(startOfthisMonth));
+              setSelectEnd(new Date(endOfthisMonth))
+              setMonth(new Date(startOfthisMonth).getMonth());
+              setDate(new Date(startOfthisMonth))
+            }
+
+            }
+            className="calendar__filter-checkbox" value="duringThisMonth" name="active-filter" type="radio"
+          />
           <div className="calendar__filter-period">
             В этом месяце
           </div>
         </label>
 
         <label>
-          <input className="calendar__filter-checkbox" value="lastMonth" name="active-filter" type="radio" />
+          <input
+            onChange={()=>{
+              let startOfLastMonth = now.subtract(1,'month').startOf('month')._d.toString();
+              let endOfLastMonth = now.endOf('month')._d.toString();
+              console.log(startOfLastMonth, endOfLastMonth)
+              setSelectStart(new Date(startOfLastMonth));
+              setSelectEnd(new Date(endOfLastMonth))
+              setMonth(new Date(startOfLastMonth).getMonth());
+              setDate(new Date(startOfLastMonth))
+            }
+            }
+            className="calendar__filter-checkbox" value="lastMonth" name="active-filter" type="radio"
+          />
           <div className="calendar__filter-period">
             Прошлый месяц
           </div>
@@ -63,19 +132,14 @@ const FilterCalendar = () => {
       <div className="calendar__dates-block">
 
         <div className="calendar__block-calendar">
+          <div className="calendar__current-month">
+            {allMonth[month]}, {date.getFullYear()}
+          </div>
           <SimpleReactCalendar
-            MonthHeaderComponent={() => (
-              <div className="calendar__controls-top">
-                <button className="calendar__month-back month-control">
-                  <img src="/images/icon-calendar-control-arrow.svg" alt="" />
-                </button>
-                Февраль, 2020
-                <button className="calendar__month-forward month-control">
-                  <img src="/images/icon-calendar-control-arrow.svg" alt="" />
-                </button>
-              </div>
-            )}
-
+            onMonthChange={(date)=>{
+              setMonth(date.getMonth())
+              setDate(date)
+            }}
             daysOfWeek={['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс']}
             activeMonth={date}
             mode="range"
@@ -84,17 +148,30 @@ const FilterCalendar = () => {
             }}
             selected={{start: selectStart, end: selectEnd}}
             onSelect={(el) => {
-              console.log(el);
-
               setSelectStart(el.start)
 
               setSelectEnd(el.end)
             }}
             onSelectionProgress={(el) => {
+              setSelectStart(el.start)
 
+              setSelectEnd(el.end)
             }}
             blockClassName="block-calendar"
           />
+        </div>
+
+        <div className="calendar__main-controls">
+          <button
+            className={'calendar__cancel'}
+          >
+            Отмена
+          </button>
+          <button
+            className={'calendar__update'}
+          >
+            Обновить
+          </button>
         </div>
 
       </div>
